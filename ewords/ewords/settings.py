c8290f4c678fc34bccadfd940be2221e63ea08b1
Pavel Gavrilov <vrn10@ya.ru>
2020-06-12 13:03:47 +0300

    Add changes to settings for CORS policies by this instruction: https://stackoverflow.com/questions/35760943/how-can-i-enable-cors-on-django-rest-framework

--- ewords/ewords/settings.py
+++ ewords/ewords/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'words',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'ewords.urls'
@@ -123,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200',
+]
